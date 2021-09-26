64087275c6fc536d506ffd14b2b8e1c75e9fc64b
knyazevvb <ks38@mail.ru>
2021-09-26 19:07:24 +0300

    lesson2.1

--- src/main/java/lesson_2/HomeWorkApp2.java
+++ src/main/java/lesson_2/HomeWorkApp2.java
@@ -4,47 +4,40 @@ public class HomeWorkApp2 {
 
     public static void main(String[] args) {
         System.out.println("++++++++++Homework 2++++++++++");
-        System.out.println("Условие выполнения первого задания: " + within10and20());
-        isPositiveOrNegative();
-        System.out.println("Условие выполнения третьего задания: " + isNegative());
-        printWordNTimes();
-        System.out.println("Условие выполнения пятого задания: " + leapYear());
+        System.out.println("Условие выполнения первого задания: " + within10and20(10, -100));
+        isPositiveOrNegative(-15);
+        System.out.println("Условие выполнения третьего задания: " + isNegative(-1));
+        printWordNTimes("GeekBrains", 5);
+        System.out.println("Условие выполнения пятого задания: " + leapYear(1900));
     }
 
-    public static boolean within10and20() {
+    public static boolean within10and20(int a, int b) {
         System.out.println("----------Task one----------");
-        int a = 10;
-        int b = -100;
         return (a + b > 10) & (a + b <= 20);
     }
 
-    private static void isPositiveOrNegative() {
+    private static void isPositiveOrNegative(int x1) {
         System.out.println("----------Task two----------");
-        int x = -15;
-        if (x >= 0) {
-            System.out.println("Число: " + x + " положительное");
-        } else System.out.println("Число: " + x + " отрицательное");
+        if (x1 >= 0) {
+            System.out.println("Число: " + x1 + " положительное");
+        } else System.out.println("Число: " + x1 + " отрицательное");
     }
 
-    public static boolean isNegative() {
+    public static boolean isNegative(int x) {
         System.out.println("----------Task three----------");
-        int x = -1;
         return x < 0;
     }
 
-    public static void printWordNTimes() {
+    public static void printWordNTimes(String word, int times) {
         System.out.println("----------Task four----------");
-        String word = "Geek Brains";
-        int times = 5;
         System.out.println("Слово " + word + " будет повторяться " + times + " раз.");
         for (int i = 0; i < times; i++) {
             System.out.println(" Слово: " + word);
         }
     }
 
-    public static boolean leapYear() {
+    public static boolean leapYear(int birthday) {
         System.out.println("----------Task five*----------");
-        int birthday = 1900;
         return ((birthday % 4 == 0) || (birthday % 400 == 0)) && (birthday % 100 != 0);
     }
 }
